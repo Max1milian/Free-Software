5865cf3dea48f45ed363bc9c96ba26bf205ae25a
Maximilian Stocker <max.stocker@gmx.de>
2025-07-29 12:24:10 +0200

    added javascript

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 async function loadPage(page) {
     try {
       const response = await fetch(page);
-      if (!response.ok) {
+      console.log(response);
+      /*if (!response.ok) {
         throw new Error(`Seite ${page} konnte nicht geladen werden: ${response.status}`);
-      }
+      }*/
       const content = await response.text();
       document.getElementById('content').innerHTML = content;
     } catch (error) {
       document.getElementById('content').innerHTML = `<p style="color:red;">Fehler: ${error.message}</p>`;
     }
   }
-  
+
+  window.onload = () => loadPage('index');
